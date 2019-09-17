c5e5e0753ff0d1218c63f1f982ee57496b2a5c8e
Cosby1992 <Ande016j@edu.easj.dk>
2019-09-17 11:22:48 +0200

    Added comments to FileUtils

--- src/model/FileUtils.java
+++ src/model/FileUtils.java
@@ -5,11 +5,15 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 
+/**
+ * This class handles file saving and file reading
+ */
 public class FileUtils {
 
-
+    //Writes file to a path taken as argument
     public static void writeToFile(String path, byte[] fileBytes) throws IOException {
 
+        //writing file content bytes to path
         FileOutputStream fos = new FileOutputStream(path);
         fos.write(fileBytes);
         fos.close();
@@ -17,14 +21,17 @@ public class FileUtils {
     }
 
 
+    //Reads file using the path of a File and returns the content as byte-array
     public static byte[] readFile(File file) throws IOException {
         return Files.readAllBytes(file.toPath());
     }
 
+    //Reads file using the path of a File and returns the content as String
     public static String getBytesAsString(File file) throws IOException {
 
         StringBuilder builder = new StringBuilder();
 
+        //using read file method that returns a byte-array
         for (byte b : readFile(file)) {
             builder.append(b);
         }
@@ -33,6 +40,7 @@ public class FileUtils {
 
     }
 
+    //Transforming byte-array (from file) to String using StringBuilder
     public static String getStringFromByteArray(byte[] bytes){
         StringBuilder builder = new StringBuilder();
 
